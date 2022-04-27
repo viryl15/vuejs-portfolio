6a1894ccc8b9e2a71db4e85784bd1f4e070e71ef
viryl scott <viryl1515@gmail.com>
2022-04-27 05:29:08 +0100

    ci/cd

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,17 @@
 set -e
 
 # build
-npm run build
+#npm run build
 
 # navigate into the build output directory
 cd dist
 
 git init
+git config --global user.email "viryl1515@gmail.com"
+git config --global user.name "viryl15"
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:viryl15/vuejs-portfolio.git master:gh-pages
+git push -f git@github.com:viryl15/vuejs-portfolio.git main:gh-pages
 
 cd -
