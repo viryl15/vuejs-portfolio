8851bd403cfa9e6a29b892e04e2e28aa96aa7c7d
viryl scott <42947084+viryl15@users.noreply.github.com>
2025-01-11 08:01:50 +0100

    add data.js

---
+++ src/data.js
@@ -0,0 +1,317 @@
+const userData = {
+  user: {
+    name: "Dylan Zambou",
+    pseudo: "Viryl Scott",
+    status: "Full-Stack Developer",
+    email: "viryl1515@gmail.com",
+    phone: "0751267665",
+    city: "Paris",
+    lang: "French, English",
+    photo: "assets/img/photo.jpg",
+  },
+  description: {
+    title: "About me",
+    description: "Hope to meet you soon",
+    pres_title: "Who Am I?",
+    pres_first: "I am a full-stack software developer with 4 years of experience. Proficient in various web development technologies such as Laravel, Symfony, VueJS, NuxtJS, Next.js, Angular, NodeJS, TailwindCSS and languages like JavaScript, PHP, TypeScript. Experienced in working with databases like MySQL, Sequelize, Neo4j, SQLite and MongoDB, as well as CMSs like Wordpress. Skilled in Git for version control and collaboration. Proven ability to deliver high-quality and effective solutions in a fast-paced environment. I'm also an open-source enthusiast, passionate about contributing to and learning from the community.",
+    pres_second: "",
+  },
+  experiences: {
+      title: "Experience",
+      description: "Profesional And Academic",
+      academic: [
+            {
+              "year": "2023 - 2024",
+              "title": "Computer Science Master at F2I",
+              "content": "Expert Architecte Web, France"
+            },
+            {
+              "year": "2016 - 2019",
+              "title": "IT License",
+              "content": "SUP'PTIC, Cameroon"
+            },
+            {
+              "year": "2010 - 2016",
+              "title": "Scientific Baccalaureate",
+              "content": "Lycée de Bafou, Camerooon"
+            }
+          ],
+      professional: [
+            {
+              "year": "01.2020 - 02.2022",
+              "title": "Full-stack Developer (Laravel/Vue.js) at <a href='https://gohze.com/'>Gohze</b>",
+              "content": "Design and build web and Desktop applications using: <ul><li>Laravel</li><li>Laravel-Nova</li><li>Nodejs</li><li>JavaScript</li><li>Vue.js and VueX</li><li>Nuxt.js</li><li>TailwindCSS</li><li>Vue.js/Electron-vue</li><li>Docker</li><li>Wordpress</li><li>JAVA/JavaFX</li><li>Maven</li><li>HTML5|CSS3|SASS|JQuery</li></ul>"
+            },
+            {
+              "year": "03.2022 - 08.2022",
+              "title": "PHP Developer",
+              "content": "Design and build web applications"
+            },
+            {
+              "year": "09.2022 - 04.2023",
+              "title": "PHP Developer (Symfony) at <a href='https://vacancysoft.com/'>Vacancysoft</a>",
+              "content": "Design and build Web application using: <ul><li>Symfony 5</li><li>Angular 12</li><li>Nodejs</li><li>JavaScript</li><li>Bootstarp 5</li><li>Chart.js and plugins</li><li>SASS|JQuery</li></ul>"
+            },
+            {
+              "year": "05.2023",
+              "title": "Full-stack Developer (PHP - JavaScript)",
+              "content": "Consultant at <a href='https://youpi.cm/'>Youpi Sarl</a> (freelancer)."
+            },
+            {
+              "year": "02.2024 - 12.2024",
+              "title": "Project Manager / Full-stack Developer",
+              "content": "Project Manager at <a href='https://webavenue.agency/'>WebAvenue</a> <br> <ul><li>Mobile application development with Flutter</li><li>Customize WordPress themes using PHP and CSS (Elemmentor)</li><li>Maintain customer websites</li></ul>"
+            }
+        ],
+    },
+  skills: [
+            {
+              "title": "Laravel",
+              "img": "laravel.png"
+            },
+            {
+              "title": "Symfony",
+              "img": "symfony-black.png"
+            },
+            {
+              "title": "PHP",
+              "img": "php.png"
+            },
+            {
+              "title": "JavaScript",
+              "img": "javascript-logo.jpg"
+            },
+            {
+              "title": "Vue.js",
+              "img": "vuejs.png"
+            },
+            {
+              "title": "VueX",
+              "img": "vuex.png"
+            },
+            {
+              "title": "Nuxtjs",
+              "img": "nuxt.js-logo.jpg"
+            },
+            {
+              "title": "Angular JS",
+              "img": "angular.png"
+            },
+            {
+              "title": "Docker",
+              "img": "docker-logo.png"
+            },
+            {
+              "title": "Bootstrap",
+              "img": "bootstrap-logo.png"
+            },
+            {
+              "title": "TailwindCSS",
+              "img": "tailwindcss.png"
+            },
+            {
+              "title": "Nodejs",
+              "img": "nodejs.png"
+            },
+            {
+              "title": "Python",
+              "img": "python.png"
+            },
+            {
+              "title": "Arduino",
+              "img": "arduino.png"
+            },
+            {
+              "title": "Wordpress",
+              "img": "WordPresslogoiconblue.png"
+            },
+            {
+              "title": "Balsamiq-Wireframes",
+              "img": "Balsamiq-Wireframes-logo.png"
+            },
+            {
+              "title": "Gitlab",
+              "img": "gitlab-logo.png"
+            },
+            {
+              "title": "C",
+              "img": "C.png"
+            },
+            {
+              "title": "Flutter",
+              "img": "flutter.png"
+            },
+            {
+              "title": "Java",
+              "img": "java.png"
+            },
+            {
+              "title": "HTML",
+              "img": "html.png"
+            },
+            {
+              "title": "CSS",
+              "img": "css.png"
+            },
+            {
+              "title": "PHP",
+              "img": "php.png"
+            },
+            {
+              "title": "Laravel",
+              "img": "laravel.png"
+            },
+            {
+              "title": "VueJS",
+              "img": "vuejs.png"
+            },
+            {
+              "title": "Android",
+              "img": "android.png"
+            },
+            {
+              "title": "Linux",
+              "img": "linux.png"
+            },
+            {
+              "title": "Git/Github",
+              "img": "Octocat.png"
+            },
+            {
+              "title": "VPS/Web Hosting",
+              "img": "ovh.png"
+            }
+          ],
+  links: {
+    facebook: "https://www.facebook.com/rovel.tematio/",
+    instagram: "https://www.instagram.com/viryl15/",
+    linkedin: "https://www.linkedin.com/in/rovel-dylan-tematio-zambou-596303172/",
+    github: "https://github.com/viryl15",
+    stackoverflow : "https://stackoverflow.com/users/12225021/viryl15",
+  },
+  posts: [],
+  projects: [
+    {
+      "title": "YoupiJob Mobile App",
+      "description": "YoupiJob is a job search platform based in Cameroon, designed to help job seekers and employers get in touch. It offers a user-friendly interface enabling candidates to view and apply to a variety of job offers, while companies can post their ads to attract qualified talent. \n\n<br><br>\n\nMobile App LINK: <a href='https://play.google.com/store/apps/details?id=cm.youpi.youpijob' target='_blank'>YoupiJob mobile app</a>",
+      "delivery_date": "2024-09-01",
+      "image": "youpijob-mobile.png",
+      "color": "blue",
+      "tag": "Flutter|dart|GetX|Dio|Socket.io|Laravel/RestAPI",
+    },
+    {
+      "title": "YoupiJob",
+      "description": "YoupiJob is a job search platform based in Cameroon, designed to help job seekers and employers get in touch. It offers a user-friendly interface enabling candidates to view and apply to a variety of job offers, while companies can post their ads to attract qualified talent. \n\n<br><br>\n\nWeb LINK: <a href='https://youpijob.cm/' target='_blank'>YoupiJob web app</a>",
+      "delivery_date": "2024-05-01",
+      "image": "youpijob.png",
+      "color": "blue",
+      "tag": "Laravel|Next.js/React.js|php|REST API|JS|hosting|SEO|git|Websocket",
+    },
+    {
+      "title": "Le Japap",
+      "description": "Le Japap is a blog to talk about Cameroon and sub-region news.\n\n<br><br>\n\nLINK: <a href='https://lejapap.net/' target='_blank'>Le Japap</a>",
+      "delivery_date": "2024-01-04",
+      "image": "lejapap.png",
+      "color": "grey",
+      "tag": "wordpress|Divi|php|css|JS|hosting|SEO",
+    },
+    {
+      "title": "YOUPI SARL",
+      "description": "YOUPI SARL is a digital agency for the design and development of quality websites and web applications.\n\n<br><br>\n\nLINK: <a href='https://youpi.cm/' target='_blank'>YOUPI SARL</a>",
+      "delivery_date": "2023-08-10",
+      "image": "youpi.cm.png",
+      "color": "orange",
+      "tag": "Nuxtjs|TailwidCSS|Laravel/RestAPI|php|hosting|SEO",
+    },
+    {
+      "title": "TAILWAVE",
+      "description": "Tailwave is a collection of meticulously crafted templates designed to accelerate your front-end development workflow.\n\n<br><br>\n\nLINK: <a href='https://tailwave.io/' target='_blank'>Tailwave UI</a>",
+      "delivery_date": "2023-08-01",
+      "image": "tailwave-landing-head.png",
+      "color": "purple",
+      "tag": "Nuxt.js|TailwindCSS|SASS|CSS|JAVASCRIPT|HOSTING|GIT|GITHUB",
+    },
+    {
+      "title": "Screel",
+      "description": "A platform for tech enthusiasts and developers to come together and connect. Here, we can share our wildest and most exciting tech projects, bond over our shared love of all things nerdy and geeky, and connect with local developers. Screel is <b>open source</b>\n\n<br><br>\n\nLINK: <a href='https://screel.vercel.app/' target='_blank'>Screel App</a>",
+      "delivery_date": "2023-01-21",
+      "image": "screel-app.png",
+      "color": "purple",
+      "tag": "vue.js|laravel|php|TailwindCSS|Hosting|Docker|mongodb|Rest-api",
+    },
+    {
+      "title": "Digital Square Group",
+      "description": "Digital-squaregroup is the website of Digital Square company. The website presents the company activities, the services, the team and its delivered projects. \n\n<br><br>\n\nLINK: <a href='https://digital-squaregroup.com/' target='_blank'>Digital-Square group </a>",
+      "delivery_date": "2022-09-09",
+      "image": "digitalsquaregroup.png",
+      "color": "blue",
+      "tag": "wordpress|Divi|php|css|JS|hosting",
+    },
+    {
+      "title": "Waterlife",
+      "description": "Waterlife is a network marketing system build using Laravel Framework and Laravel-nova and MySQL.\nI worked on this project as a project lead and i still work on it as a maintainer\n\n<br><br>\n\nLINK: <a href='https://dev.waterlife.cm' target='_blank'>Waterlife</a>",
+      "delivery_date": "2022-03-31",
+      "image": "waterlife-dashboard.png",
+      "color": "blue",
+      "tag": "laravel/laravel-nova|MySQL|hosting|git|gitlab",
+    },
+    {
+      "title": "Zen family",
+      "description": "Zen family is an e-commerce mobile app.\nBuild using <b>flutter</b>, <b>Laravel framework</b> for the back-end, and <b>Laravel-nova</b> for the back-office.\nI worked on this project as a back-end developer by coding the api and make the dashboard\n\n<br><br>\n\nLINK: <a href='https://play.google.com/store/apps/details?id=com.gohze.zen_family&pcampaignid=web_share'> <b>Mobile app</b></a>",
+      "delivery_date": "2022-02-15",
+      "image": "zen-family.png",
+      "color": "white",
+      "tag": "Flutter|laravel|laravel-nova|hosting|git|gitlab",
+    },
+    {
+      "title": "Inheritance-tax",
+      "description": "Inheritance-tax is an application made to facilitate the work of tax advisors in the management of wealth.\nBuild using <b>electron-vue</b> (<i>the librairie used to build desktop apps using vue.js framework</i>), <b>Nodejs</b> and <b>Laravel framework</b> for the back-end.\nI worked on this project as a Full-Stack Developer.\n\n<br><br>\n\nLINK: <b>Desktop app</b>",
+      "delivery_date": "2022-02-02",
+      "image": "inheritance-tax.png",
+      "color": "green",
+      "tag": "vue|chart.js|tailwindcss|electron-vue|Nodejs|sequelize|laravel|laravel-nova|git|gitlab",
+    },
+    {
+      "title": "Mobilepartner",
+      "description": "Mobilepartner is an application that allows users to buy phone credit through any operator in Cameroon.\nBuild using <b>Vue.js/VueX</b> and <b>Laravel framework</b> for the back-end and Laravel-nova for the back-office.\nI worked on this project as a Full-Stack Developer.\n\n<br><br>\n\nLINK: <a href='http://mpartner.cm'>mobilepartner</b>",
+      "delivery_date": "2021-12-28",
+      "image": "mobilepartner.png",
+      "color": "yellow",
+      "tag": "JS|vue.js|vuex|laravel|laravel-nova|git|gitlab",
+    },
+    {
+      "title": "MonTamponEncreur",
+      "description": "Developed Web API backend using .Net. Used Mysql server for data storage Used GitLab for application versioning Developed front end using JavaScript, Nuxtjs, VueX, HTML5, css, css3 and sass. \n I worked on this project as a front end developer; I implemented interfaces from a model; we were 2 in the integration team.\nUsing Windows operating system.\n\n<br><br>\n\nLINK: <a href='https://montamponencreur.com/' target='_blank'>montamponencreur</a>",
+      "delivery_date": "2021-09-18",
+      "image": "mtmpecr.png",
+      "color": "blue",
+      "tag": "JS|Vue.js|sass|git|gitlab",
+    },
+    {
+      "title": "OPEN-EXAM",
+      "description": "Led a team of 6 interns in building an e-learning application. Develop Web API backend using LARAVEL, php, and JWT for user authentication. Use Neo4j for data storage; implement auto-evaluation of student on the site. Developed front end using JavaScript, Nuxtjs, axios, Vue.js, VueX, HTML5, css, css3. Integrated with smtp for email notification.\nUsing Linux (Ubuntu) and windows operating system.\n\n<br><br>\n\nLINK: <a href='https://open-exam.com/' target='_blank'>OPEN-EXAM</a>",
+      "delivery_date": "2021-02-25",
+      "image": "open-exam.png",
+      "color": "blue",
+      "tag": "LARAVEL |JS| Nuxt.js | AXIOS | Vue.js | VueX | Neo4j|sass|git|gitlab",
+    },
+    {
+      "title": "Youpi Exchange",
+      "description": "Youpi Exchange is a platform that allows any user to buy and sell bitcoins directly online,without creating an account. With its fee structure, it is possible to exchange even a very small amount of bitcoin.The security of the site is its main strength, so you can make your transactions without the risk of being hacked,which makes Youpi Exchange reliable. Developed web app frontend using JavaScript Vuejs, VueX Developed a maintainable RESTful api using Laravel PHP and data storage in MySQL.\nI worked on this project as a project lead and i still work on it as a maintainer.\n\n<br><br>\n\nLINK: <a href='https://youpiexchange.com/' target='_blank'>Youpi Exchange</a>",
+      "delivery_date": "2021-04-07",
+      "image": "youpiexchange.png",
+      "color": "orange",
+      "tag": "Vue.js|VueX|axios|Laravel|Laravel-nova|MySQL|hosting|git|gitlab",
+    },
+    {
+      "title": "DSF Generator",
+      "description": "DSF generator is a software developed in JAVA and JAVAFX to automatically\ngenerate the declaration and tax of a statistical\ncompany and facilitate the work of accountants.\n\n<br><br>\n\nLINK: <b>Desktop app</b>",
+      "delivery_date": "2020-05-01",
+      "image": "balance-et-DSF.png",
+      "color": "green",
+      "tag": "java|javafx|css|maven|git|gitlab",
+    },
+  ],
+}
+
+export default userData
